76ba9856db889666a9a5c1747123a69f0f5a5361
rbeninca@gmail.com <rbeninca@gmail.com>
2025-03-24 09:09:15 -0300

    imc com uma atividade apenas

--- app/src/main/java/com/ifsc/contaclicks/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclicks/MainActivity.java
@@ -3,34 +3,38 @@ package com.ifsc.contaclicks;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     int i=0;
+    EditText edpeso,edaltura;
+    TextView tvresulado;
+    Button buttonCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Associando objeto interface a variavel local;
-        TextView tv=findViewById(R.id.textView);
-        tv.setText(getString(R.string.app_name));
-        Button b=findViewById(R.id.button);
-        b.setOnClickListener(v -> {//Seu codigo lindo aqui
-        });
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tv.setText( Integer.toString(i)  );
-                i++;
-            }
-        });
+        edpeso=findViewById(R.id.edpeso);
+        edaltura=findViewById(R.id.edaltura);
+        tvresulado=findViewById(R.id.tvresultadoimc);
+        buttonCalcular=findViewById(R.id.button);
+        //define um tratamento para o click do botão
+        buttonCalcular.setOnClickListener(v->{
+            //Calcular o IMC
+            //recuper os daos de peso e altura
+            double peso,altura, imc;
+            peso=Double.parseDouble( edpeso.getText().toString());
+            altura=Double.parseDouble( edaltura.getText().toString());
+            imc=peso/(altura*altura);
 
+            tvresulado.setText(Double.toString(imc));
+        });
     }
+
+
 }
