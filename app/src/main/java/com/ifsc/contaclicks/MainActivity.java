e974159296c36cf5c938711937d1b2dcc978639a
Your Name <you@example.com>
2025-03-31 08:22:15 -0300

    ciclo de vida

--- app/src/main/java/com/ifsc/contaclicks/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclicks/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ifsc.contaclicks;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("ciclo de vida","metodo onCreate");
         setContentView(R.layout.activity_main);
         edpeso=findViewById(R.id.edpeso);
         edaltura=findViewById(R.id.edaltura);
@@ -40,5 +42,39 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("ciclo de vida","metodo onStart");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("ciclo de vida","metodo onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("ciclo de vida","metodo onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("ciclo de vida","metodo onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("ciclo de vida","metodo onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("ciclo de vida","metodo onRestart");
+    }
 }
