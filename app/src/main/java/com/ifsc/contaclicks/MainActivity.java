a7d4a60cb4d84a5444a73fffeea972271808797a
Your Name <you@example.com>
2025-03-31 09:02:35 -0300

    comunicação entre atividades Bundle

--- app/src/main/java/com/ifsc/contaclicks/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclicks/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ifsc.contaclicks;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -29,52 +30,11 @@ public class MainActivity extends AppCompatActivity {
         buttonCalcular=findViewById(R.id.button);
         //define um tratamento para o click do botão
         buttonCalcular.setOnClickListener(v->{
-            //Calcular o IMC
-            //recuper os daos de peso e altura
-            double peso,altura, imc;
-            peso=Double.parseDouble( edpeso.getText().toString());
-            altura=Double.parseDouble( edaltura.getText().toString());
-            imc=peso/(altura*altura);
-            //Formarmatando numero ##,##
-            DecimalFormat decimalFormat=new DecimalFormat("##.##");
-
-            tvresulado.setText( decimalFormat.format(imc));
+            Intent intent = new Intent(getApplicationContext(), MainActivityB.class);
+            String msg= edpeso.getText().toString();
+            intent.putExtra("mensagem", msg);
+            startActivity(intent);
         });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("ciclo de vida","metodo onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("ciclo de vida","metodo onResume");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("ciclo de vida","metodo onStop");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("ciclo de vida","metodo onPause");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("ciclo de vida","metodo onDestroy");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("ciclo de vida","metodo onRestart");
-    }
 }
