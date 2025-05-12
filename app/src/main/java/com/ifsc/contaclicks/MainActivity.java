f4e85e2a3e034b9f18fb3478b34c0a3ff503da23
romulo beninca <rbeninca@gmail.com>
2025-05-12 08:19:04 -0300

    item layout com image view para figura do planeta

--- app/src/main/java/com/ifsc/contaclicks/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclicks/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 nomes);
 
         lv.setAdapter(a);
+
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
